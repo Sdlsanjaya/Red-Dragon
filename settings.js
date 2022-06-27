dc2402a440e675a4d10a1fbd2cb151893843d15f
Sdlsanjaya <57230724+Sdlsanjaya@users.noreply.github.com>
2022-06-27 19:06:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,18 +20,18 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is BEAST LK🔥
+My Creator is SDL SANJAYA😎` //Costomize Alive Message (type your message in `` )
 
 global.owner = ['+380944814219'] //Owner number in aive msg
 global.premium = ['+380944814219'] //Owner Number info
 global.ownernomer = '+380944814219' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON' //Bot Name
+global.ownername = 'SDL SANJAYA' //Owner Name
+global.botname = ' BEAST LK' //Bot Name
 
-global.button = '🤭මේක ටච් කරන්න📍' //Costomize A Button Name In Alive Message
-global.btnurl = '🙂🔥දැන් සැපද?😑' //Change Your Button's URL
+global.button = '🔥 WhatsApp' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://chat.whatsapp.com/FKNkuh4lDbh5hgW9MDgyZm' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
@@ -39,11 +39,11 @@ global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
 global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
-global.sp = '⭔' //A Charactor In Menu!
+global.sp = '💸' //A Charactor In Menu!
 
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
-global.footer = '©_Kumuthu.' // DO NOT CHANGE!
+global.ig = 'https://www.instagram.com/sdlsanjaya/' //Github Account!
+global.footer = '©sdlsanjaya.' // DO NOT CHANGE!
 
 global.mess = { // Error Mesages
     success: 'Done ✅',
